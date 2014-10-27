2936f799fed222fa622239ff213869dbef58a374
Alvaro <varofv66@gmail.com>
2014-10-27 08:50:34 +0100

    Proyecto Person inicial, clase Person y metodo getAge

---
+++ Person.java
@@ -0,0 +1,31 @@
+
+/**
+ * Write a description of class Person here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Person
+{
+    // The age of a person
+    private int age;
+
+    /**
+     * Create a machine that shows the age of a person
+     */
+    public Person(int ageOfPerson)
+    {
+        // initialise instance variables
+        age = ageOfPerson;
+    }
+
+    /**
+     * 
+     * Return the age of a person
+     */
+    public int getAge()
+    {
+        // put your code here
+        return age;
+    }
+}
